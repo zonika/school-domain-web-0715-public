e17b471566a5456a97b410ac95c4122046c6ba76
zonika <zchodosh@conncoll.edu>
2015-08-05 09:35:42 -0400

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # Write code here
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name=name
+    @roster={}
+  end
+
+  def first_student(grade)
+    @roster[grade]
+  end
+  def add_student(name,grade)
+    if first_student(grade) != nil
+      @roster[grade]<<name
+    else
+      @roster[grade]=[name]
+    end
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    @roster.each do |grade, students|
+      students.sort!
+    end
+  end
+
+end
